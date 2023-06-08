485399572ee129b88a5cee6705fd21c4067723f0
atikur09 <atikurrahman.rk@gmail.com>
2023-06-08 18:23:18 +0800

    module 49 finished

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -13,10 +14,42 @@ const Shop = () => {
         .then(data => setProducts(data))
     }, []);
 
-    const handelAddToCart = (product) =>{
+    // code for get item from local storage and display to ui
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        for(const  id in storedCart){
+            const addedProduct = products.find(product => product.id===id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products]);
+
+    /* https://web.programming-hero.com/update-1/video/update-1-49-6-advanced-display-local-storage-cart-to-the-ui 
+    line 21 if condition explaiend in this video from 6 min
+    */
+
+    
+    const handelAddToCart = (selectedProduct) =>{
         // console.log(product);
-        const newCart = [...cart, product];
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+            selectedProduct.quantity = 1;
+            newCart = [...cart, selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+
         setCart(newCart);
+        addToDb(selectedProduct.id);
     }
 
     return (
